c6daf5a7228019b757cafdd48743576b17894c9d
Hyerim Kim <hyerimki@c10r8s3.42seoul.kr>
2022-07-25 13:56:59 +0900

    0725

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -6,28 +6,28 @@
 /*   By: hyerimki <hyerimki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 15:12:55 by hyerimki          #+#    #+#             */
-/*   Updated: 2022/07/24 15:42:02 by hyerimki         ###   ########.fr       */
+/*   Updated: 2022/07/25 13:56:38 by hyerimki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #ifndef GET_NEXT_LINE_BONUS_H
-// # define GET_NEXT_LINE_BONUS_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
-// # include <fcntl.h>
-// # include <stdlib.h>
-// # include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-// # ifndef BUFFER_SIZE
-// #  define BUFFER_SIZE 42
-// # endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
-// char	*ft_line(char *save);
-// char	*ft_read(int fd, char *arr, char *backup);
-// char	*get_next_line(int fd);
-// int		ft_strlen(const char *s);
-// char	*ft_strchr(char *s, int c);
-// char	*ft_strjoin(char const *s1, char const *s2);
-// char	*ft_strdup(const char *s1);
-// char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_line(char *save);
+char	*ft_read(int fd, char *arr, char *backup);
+char	*get_next_line(int fd);
+int		ft_strlen(const char *s);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
-// #endif
+#endif
